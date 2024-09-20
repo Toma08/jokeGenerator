13f46aa199c8ff32990e67deeb30cd482774ab13
Tóth Tamás <tothtomi08@gmail.com>
2024-09-20 20:33:57 +0200

    FirstHomeProject

---
+++ app/src/main/java/com/example/jokes/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.jokes;
+import android.graphics.Color;
+import android.widget.Button;
+import android.widget.TextView;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnNextJoke;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+    }
+    String szoveg="asafiu";
+
+    public void next(View v) {
+        btnNextJoke.setBackgroundColor(Color.RED);
+        //btnNextJoke.setText(szoveg);
+    }
+}
